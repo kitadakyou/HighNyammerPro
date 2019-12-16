d2fccf31a05dcc6aa480aa763394b856558a0b7e
Hikaru ETO <kitadakyou@yahoo.co.jp>
2019-12-16 17:34:57 +0900

    グループ一覧を表示するサイドバーを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="TimelineContainer">
-    <thread :posts=posts>
-    </thread>        
+<main>
+  <div class="columns">
+    <div id="SidemenuContainer" class="column is-3">
+      <b-menu>
+        <b-menu-list label="グループ">
+            <b-menu-item v-for="group in groups"
+                :label=group.name
+                tag="router-link"
+                :to=group.to>
+            </b-menu-item>
+        </b-menu-list>
+      </b-menu>
+    </div>
+    <div class="column is-9" id="TimelineContainer">
+      <thread :posts=posts>
+      </thread>        
+    </div>
   </div>
+</main>
 </template>
 
 <script>
@@ -14,6 +29,17 @@ export default {
   },
   data() {
     return  {
+      groups: [
+        {
+          name: "トピック",
+          to: "/group/1",
+        },
+        {
+          name: "シフト活動記録",
+          to: "/group/2",
+        }
+
+      ],
       posts: [ 
         {
           name: "江藤光",
@@ -41,6 +67,13 @@ Macの貸し出しがあったので対応しました。などなど",
 </script>
 
 <style scoped>
+  #SidemenuContainer {
+    text-align: right;
+    background: #f5f5f5;
+    padding-top: 2rem;
+    padding-left: 2rem;
+  }
+
   #TimelineContainer {
     padding: 3rem;
   }
