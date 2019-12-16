d282088a75e6d274828f50ce21bd2f7f96046743
Hikaru ETO <kitadakyou@yahoo.co.jp>
2019-12-16 18:21:09 +0900

    メニューはLayoutに分離

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,47 @@
 <template>
-  <nuxt />
+<main>
+  <div class="columns">
+    <div id="SidemenuContainer" class="column is-3">
+      <b-menu>
+        <b-menu-list label="グループ">
+            <b-menu-item v-for="group in groups"
+                :label=group.name
+                tag="router-link"
+                :to=group.to>
+            </b-menu-item>
+        </b-menu-list>
+      </b-menu>
+    </div>
+      <nuxt />
+  </div>
+</main>
 </template>
 
 <script>
 export default {
+  data() {
+    return  {
+      groups: [
+        {
+          name: "トピック",
+          to: "/group/1",
+        },
+        {
+          name: "シフト活動記録",
+          to: "/group/2",
+        }
 
+      ],
+    }
+  },
 }
 </script>
+
+<style scoped>
+  #SidemenuContainer {
+    text-align: right;
+    background: #f5f5f5;
+    padding-top: 2rem;
+    padding-left: 2rem;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,7 @@
 <template>
-<main>
-  <div class="columns">
-    <div id="SidemenuContainer" class="column is-3">
-      <b-menu>
-        <b-menu-list label="グループ">
-            <b-menu-item v-for="group in groups"
-                :label=group.name
-                tag="router-link"
-                :to=group.to>
-            </b-menu-item>
-        </b-menu-list>
-      </b-menu>
-    </div>
-    <div class="column is-9" id="TimelineContainer">
-      <thread :posts=posts>
-      </thread>        
-    </div>
+  <div class="column is-9" id="TimelineContainer">
+    <thread :posts=posts></thread>        
   </div>
-</main>
 </template>
 
 <script>
@@ -29,17 +13,6 @@ export default {
   },
   data() {
     return  {
-      groups: [
-        {
-          name: "トピック",
-          to: "/group/1",
-        },
-        {
-          name: "シフト活動記録",
-          to: "/group/2",
-        }
-
-      ],
       posts: [ 
         {
           name: "江藤光",
@@ -67,13 +40,6 @@ Macの貸し出しがあったので対応しました。などなど",
 </script>
 
 <style scoped>
-  #SidemenuContainer {
-    text-align: right;
-    background: #f5f5f5;
-    padding-top: 2rem;
-    padding-left: 2rem;
-  }
-
   #TimelineContainer {
     padding: 3rem;
   }
